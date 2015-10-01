28c9fa7258601dbf4b482756c19eabdb443211d8
johnquiwa <jcq211@nyu.edu>
2015-09-30 20:06:58 -0400

    fixed asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w('gmap.js')
+Rails.application.config.assets.precompile += %w( gmap.js )
+Rails.application.config.assets.precompile += %w( css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( css/stylish-portfolio.css )
